aa91a37b349af965db0573deb28b6b42d2e5dc71
“Timofey-Klimov” <Timofey_Klimov@Dell.com>
2021-05-25 02:28:15 +0300

    initial commit

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -1,4 +1,21 @@
-//
-// Created by tim on 5/24/21.
-//
+/**
+ * @author krezefal
+ * Project on GitHub: https://github.com/krezefal/MD5
+ */
 
+#include <iostream>
+
+#include "../md5/MD5.hpp"
+
+int main() {
+
+    std::string message;
+    std::cout << "Input message to hashing: ";
+    std::cin >> message;
+
+    MD5 md5;
+    std::cout << "MD5 hash: ";
+    std::cout << md5.calcHash(message);
+
+    return 0;
+}
